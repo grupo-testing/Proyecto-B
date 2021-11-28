4f805810c69b4a6d969b32484773c02ebdd51fcc
Macarena Concha <macarena.concha@plaglabs.com>
2021-11-28 19:43:07 -0300

    feat: added reservation style

--- cinema/app/helpers/reservations_helper.rb
+++ cinema/app/helpers/reservations_helper.rb
@@ -1,2 +1,14 @@
 module ReservationsHelper
+  def get_seat_label(seat)
+    number = seat%12 == 0 ? 12 : seat%12 
+    if seat <= 12
+      "A-#{number}"
+    elsif seat <= 24
+      "B-#{number}"
+    elsif seat <= 36
+      "C-#{number}"
+    elsif seat <= 48
+      "D-#{number}"
+    end
+  end
 end
